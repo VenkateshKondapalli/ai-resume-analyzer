e7b63ae7f75ed841be6cf0ba5d9b4e36cc77f23c
venkatesh <venkateshkondapall18k@gmail.com>
2025-12-15 22:34:39 +0530

    adding match_label in match score

--- AI_Resume_Analyzer Frontend/src/components/MatchScore.jsx
+++ AI_Resume_Analyzer Frontend/src/components/MatchScore.jsx
@@ -1,16 +1,20 @@
 const MatchScore = ({ score }) => {
   let colorClass = "text-gray-600 border-gray-400";
   let ringColor = "ring-gray-400";
+  let match_label = "No Analysis";
 
-  if (score >= 80) {
+  if (score >= 70) {
     colorClass = "text-green-600 border-green-500";
     ringColor = "ring-green-400";
-  } else if (score >= 50) {
+    match_label = "✅ Strong Match";
+  } else if (score >= 40) {
     colorClass = "text-yellow-600 border-yellow-500";
     ringColor = "ring-yellow-400";
+    match_label = "⚠️ Partial Match";
   } else {
     colorClass = "text-red-600 border-red-500";
     ringColor = "ring-red-400";
+    match_label = "❌ Poor Match";
   }
 
   return (
@@ -21,6 +25,7 @@ const MatchScore = ({ score }) => {
         <span className="text-4xl font-extrabold">{score}%</span>
       </div>
       <p className="mt-3 text-lg font-semibold text-gray-700">Match Score</p>
+      <p className="font-medium">{match_label}</p>
     </div>
   );
 };
